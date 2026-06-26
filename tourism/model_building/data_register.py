f5b2c4e6f5f4b7d5992dd3e436d7468218e0645f
divyavarmamisc <divyavarma.misc@gmail.com>
2026-06-26 15:24:21 +0000

    first commit

--- tourism/model_building/data_register.py
+++ tourism/model_building/data_register.py
@@ -8,12 +8,7 @@ repo_id = "divyavarmamisc/tourism-prediction"
 repo_type = "dataset"
 
 # Initialize API client
-# Fetch token from Colab secrets
-raw_token = userdata.get('HF_TOKEN')
-print(f"Raw token from secrets: '{raw_token}'")
-token = raw_token.strip() # .strip() added to remove leading/trailing whitespace
-print(f"Stripped token: '{token}'")
-api = HfApi(token=token)
+api = HfApi(token=os.getenv("HF_TOKEN"))
 
 # Step 1: Check if the space exists
 try:
@@ -23,10 +18,6 @@ except RepositoryNotFoundError:
     print(f"Space '{repo_id}' not found. Creating new space...")
     create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
     print(f"Space '{repo_id}' created.")
-except HfHubHTTPError as e:
-    print(f"An error occurred during repo info or creation: {e}")
-    if "401 Unauthorized" in str(e):
-        print("Please check your Hugging Face token. It might be invalid or missing or lack write permissions.")
 
 api.upload_folder(
     folder_path="tourism/data",
