579fd3d93e5fcca80f2e338d456b77dba37128cf
divyavarmamisc <divyavarma.misc@gmail.com>
2026-06-27 02:13:35 +0000

    first commit

--- tourism/hosting/hosting.py
+++ tourism/hosting/hosting.py
@@ -3,8 +3,8 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="week_3_mls/deployment",     # the local folder containing your files
-    repo_id="praneeth232/Machine-Failure-Prediction",          # the target repo
+    folder_path="tourism/deployment",     # the local folder containing your files
+    repo_id="divyavarmamisc/toursim-prediction",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
